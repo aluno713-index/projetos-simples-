8d4a91c05bcf092bf9074b8642944b3ac2b2cc7a
aluno713-index <aluno7marciel13@gmail.com>
2026-07-02 17:11:25 -0300

    criação de projeto de java, modelo simple
    criação de projeto de java, modelo simples para imprimir a frase famosa "hello, world" e uma frase a mais para imprimir

---
+++ testeJAVA.java
@@ -0,0 +1,6 @@
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+        System.out.println("Este é o meu primeiro arquivo HTML hospedado usando o GitHub Pages.");
+    }
+}
